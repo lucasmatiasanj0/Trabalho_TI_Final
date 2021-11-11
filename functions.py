9a283a4181e9170c1afeb95a4f748abc99ab5e8a
LUCONAS02 <76904479+LUCONAS02@users.noreply.github.com>
2021-11-11 15:00:24 +0000

    Acabei o ex_6  e apenas falta otimizar o 5

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math as m
 
+
 def create_src():
     alfabeto=np.arange(10)
     src=np.random.randint(0,10,20)
@@ -12,10 +13,24 @@ def count(src,alfabeto):
     for i in alfabeto:
         index_count.append(np.count_nonzero(src==i))
 
-    return np.array(index_count)
+    return np.array(index_count) 
 
+def count_improved(src,alfabeto):
+    index_count_dict = {}
+
+    
+    for i in src:
+        if i not in index_count_dict:
+            index_count_dict.setdefault(i,1)
+            
+        else:
+            index_count_dict.update({i:index_count_dict[i]+1})
+            
+    return np.array(list(index_count_dict.values()))
+    
 
 def show_graphic(index_count,alfabeto_):
+    
     
     plt.bar(alfabeto_,index_count)
     plt.xlabel("Alfabeto")
@@ -36,93 +51,69 @@ def entalphy(index_count):
 
 """
 def entalphy(index_count):
-
     
     index_count_prob = index_count/np.sum(index_count)
     index_count_prob = np.delete(index_count_prob,np.where(index_count_prob==0))
-    
-    index_count = index_count/np.sum(index_count)
-    index_count = np.delete(index_count,np.where(index_count==0))
-    index_count = np.log2(index_count)
-    index_count = index_count*index_count_prob
-    entalphy_= -1*np.sum(index_count)
-    
+    entalphy_= -1*np.sum(np.log2(index_count_prob)*index_count_prob)
 
     return entalphy_
     
 
 def entalphy_X_Y(target,query,alfabeto):
-
-    # Y=query
-    # X = target
+    
+    target = np.array(target)
     query = np.array(query)
-    target= np.array(target)
-    print(query)
-    print(target)
-
-    entalphy(count(query,alfabeto))
-
-    """
-    entalphy_query = entalphy(count(query,alfabeto))
-
-    interception = np.setdiff1d(query,target)
-    slash = entalphy(count(query,interception))
-    print(entalphy_query-slash)
-    print(interception)
-    """
-
-
-    interception = np.intersect1d(query,target)
-    index_count_interception = count(query,interception)+count(target,interception)
-    print(interception)
-    print(index_count_interception)
-    print(entalphy(index_count_interception))
     
-"""
-    print(entalphy(count(target,alfabeto))+entalphy_query - 2.1219)
+    index_Y = count(query,alfabeto)
+    index_X = count(target,alfabeto)
+    entalphy_Y = entalphy(index_Y)
+    entalphy_X = entalphy(index_X)
     
-    query_slash_target = np.setdiff1d(query,target)
+    # Info Mut = H(X)+H(Y)-H(X,Y)
     
-    index_count_query_slash_target = count(query,query_slash_target)
+    #intersect=[]
+    alfabeto_ = {}
     
-    entalphy_query_slash_target = entalphy(np.array(index_count_query_slash_target))
-    
-    final_entalphy = entalphy_query-entalphy_query_slash_target
-    
+    for i in range(len(query)):
 
-"""
-"""
-    entalphy_Y = entalphy(index_count_Y)
-    entalphy_X = entalphy(index_count_X)
-
-    y_x = np.setdiff1d(Y,X)
+        new_tuple = (query[i],target[i])
+        #intersect.append(new_tuple)
+        
+        if new_tuple in alfabeto_:
+            alfabeto_.update({new_tuple:alfabeto_[new_tuple]+1})
+        
+        else:
+            alfabeto_.setdefault(new_tuple,1)
+            
+        
+    index_count = np.array(list(alfabeto_.values()))
+    intersect_entalphy = entalphy(index_count) 
     
-    print(y_x)
+    return entalphy_X+entalphy_Y-intersect_entalphy
+    #index_count = count_improved(intersect,alfabeto)
+    #print(index_count)
+    #intersect_entalphy = entalphy(index_count) 
     
-    index_count_y_inter = count(Y,y_x)
-    index_count_x_inter = count(X,y_x)
-    index_count_inter = index_count_y_inter+index_count_x_inter 
-    print(index_count_inter)  
-    entalphy_y_x = entalphy(index_count_inter)
-    print(entalphy_y_x)
-
-    final_entaphy = (entalphy_Y+entalphy_X) - entalphy_y_x
-    print(final_entaphy)
-"""
     
-
-
+    
+    
+    
+    
+        
+    
 def average(index_count,length):
-
+    
     index_count = index_count/np.sum(index_count)
-    
     index_count = index_count*np.array(length)
-
     print(np.sum(index_count))
-    
 
-#entalphy_X_Y([6,8,9,7,2,4,9,9,4,9],[2,6,4,10,5,9,5,8,0,8],np.arange(11))
+#query = np.array([2,6,4,10,5,9,5,8,0,8])
+#target = np.array([6,8,9,7,2,4,9,9,4,9,1,4,8,0,1,2,2,6,3,2,0,7,4,9,5,4,8,5,2,7,8,0,7,4,8,5,7,4,3,2,2,7,3,5,2,7,4,9,9,6])
+#alfabeto = [0,1,2,3,4,5,6,7,8,9,10]
+
+#print(entalphy_X_Y(target,query,))
 #print(np.histogram([6,8,9,7,2,4,9,9,4,9],[2,6,4,10,5,9,5,8,0,8],bin))
+
     
     
     
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scipy.io import wavfile
 import numpy as np
 import string
 import huffmancodec as huf
+import matplotlib.pyplot as plt
 
 def ex_1():
     src_and_alfabeto=f.create_src()
@@ -30,7 +31,7 @@ def ex_3_and_ex_4(file_dir,ex_4):
         src=np.array(Image.open(file_dir)).flatten()
         counted_=f.count(src,_alfabeto)
         if ex_4==0:
-            f.entalphy(counted_)
+            print(f.entalphy(counted_))
             f.show_graphic(counted_,_alfabeto)
 
         else:
@@ -47,7 +48,7 @@ def ex_3_and_ex_4(file_dir,ex_4):
         counted_=f.count(src,alfabeto)
 
         if ex_4==0:
-            f.entalphy(counted_)
+            print(f.entalphy(counted_))
             f.show_graphic(counted_,alfabeto)
         
         else:
@@ -73,74 +74,178 @@ def ex_3_and_ex_4(file_dir,ex_4):
         counted_=f.count(src,alfabeto)
 
         if ex_4==0:
-            f.entalphy(counted_)
+            print(f.entalphy(counted_))
             f.show_graphic(counted_,alfabeto_letras)
         
         else:
             counted_= np.array(list(filter(lambda a: a!=0 , counted_)))
             codec = huf.HuffmanCodec.from_data(src)
             symbols,lengths = codec.get_code_len()
+            
             f.average(counted_,lengths)
 
-def ex_5(src):
+def ex_5(file_dir):
     inicial=0
-    final=2
+    final=2 
+    file_name=file_dir.split("\\")[-1]
+    file_extension=file_name.split(".")[-1]
 
+    if file_extension =="txt":
     
-    file=open("data\\english.txt","r")
-    src=file.read()
-    src= list(map(ord,list(src))) #para aplicar a função ord em todos os elementos da lista,depois converto para lista pois é me devolvido um ponteiro para a lista
-    src= list(filter(lambda a: (91>a>=65) or (123>a>=91) , src))
-    src= list(map(chr,list(src)))
-    src = list(map(str,list(src)))
-    
-    src_final=[]
-    src_letras=[]
-    alfabeto=[]
-    while final <= len(src):
-
-        src_final.append("".join(list(map(str,list(map(ord,src[inicial:final]))))))
-
-        if "".join(list(map(str,list(map(str,src[inicial:final]))))) not in src_letras:
+        file=open(file_dir,"r")
+        src=file.read()
+        src= list(map(ord,list(src))) 
+        src= list(filter(lambda a: (91>a>=65) or (123>a>=91) , src))
+        src= list(map(chr,list(src)))
+        
+        src_final=[]
+        src_letras=[]
+        alfabeto=[]
+        
+        while final <= len(src):
+            ord_joined = int("".join(list(map(str,list(map(ord,src[inicial:final]))))))
+            letras_joined = "".join(src[inicial:final])
+            src_final.append(ord_joined)
             
-            src_letras.append("".join(list(map(str,list(map(str,src[inicial:final]))))))
-            alfabeto.append("".join(list(map(str,list(map(ord,src[inicial:final]))))))
+            if  letras_joined not in src_letras:
+                
+                src_letras.append(letras_joined)
+                alfabeto.append(ord_joined)
 
-        final+=2
-        inicial=final-2
-    
+            
+            final+=2
+            inicial = final-2
+            
 
-    src_final = np.array(list(map(int,src_final)))
-    alfabeto = np.array(list(map(int,alfabeto)))
-    alfabeto_letras = np.array(src_letras)
+        src_final = np.array(src_final)
+        alfabeto = np.array(alfabeto)
+        alfabeto_letras = np.array(src_letras)
+        
+        index_count = f.count(src_final,alfabeto)
+        print(f.entalphy(index_count))
+        f.show_graphic(index_count,alfabeto_letras)
     
-    print(alfabeto)
-    print(src_final)
-    index_count = f.count(src_final,alfabeto)
-    f.show_graphic(index_count,alfabeto_letras)
+    if file_extension == "bmp":
+
+        src = list(np.array(Image.open(file_dir)).flatten())
+        src = list(map(str,src))
+
+        src_final = []
+        alfabeto = []
+
+        while final <= len(src):
+            
+            joined = int("".join(src[inicial:final]))
+            src_final.append(joined)
+            
+            if joined not in alfabeto:
+                alfabeto.append(joined)
+
+            
+            final+=2
+            inicial = final-2
+
+        src_final = np.array(src_final)
+        alfabeto = np.array(alfabeto)
+        index_count = f.count(src_final,alfabeto)
+        print(index_count)
+        print(alfabeto)
+        print(f.entalphy(index_count))
+        #f.show_graphic(index_count,np.array(list(map(str,alfabeto))))
+        f.show_graphic(index_count,alfabeto)
+
+    if file_extension == "wav":
+
+        samplerate, src = wavfile.read(file_dir)
+        src = list(map(str,src))
+
+        src_final = []
+        alfabeto = []
+
+        while final <= len(src):
+            joined = int("".join(src[inicial:final]))
+            src_final.append(joined)
+            
+            if joined not in alfabeto:
+                alfabeto.append(joined)
+
+            final+=2
+            inicial = final-2   
+
+        src_final = np.array(src_final)
+        alfabeto = np.array(alfabeto)
+        index_count = f.count(src_final,alfabeto)
+        
+        print(src_final)
+        print(alfabeto)
+        print(f.entalphy(index_count))
+        print(index_count)
+        #f.show_graphic(index_count,np.array(list(map(str,alfabeto))))
+        f.show_graphic(index_count,alfabeto)
 
 
 def ex_6_a(query,target,alfabeto,passo):
+    
     index_final = len(query)
     index_inicial = 0
+    infmut = []
     
     while index_final <= len(target):
         compare=target[index_inicial:index_final]
         index_final+=passo
         index_inicial+=passo
-        f.entalphy_X_Y(compare,query,alfabeto)
-        
+        infmut.append(round(f.entalphy_X_Y(compare,query,alfabeto),4))
         
     
+    return np.array(infmut)
 
+def ex_6_b():
+    #target01_repeat = [6,8,9,7,2,4,9,9,4,9,1,4,8,0,1,2,2,6,3,2,0,7,4,9,5,4,8,5,2,7,8,0,7,4,8,5,7,4,3,2,2,7,3,5,2,7,4,9,9,6]
+    #saxriff = [2,6,4,10,5,9,5,8,0,8]
+    
+    samplerate, guitarSolo = wavfile.read("data\\guitarSolo.wav")
+    
+    samplerate, target01_repeat = list(wavfile.read("data\\target01 - repeat.wav"))
+    samplerate, target02_repeatNoise = list(wavfile.read("data\\target02 - repeatNoise.wav"))
+    
+    f_01 = ex_6_a(guitarSolo,target01_repeat,np.arange(256),round(len(guitarSolo)/4))
+    f_02 = ex_6_a(guitarSolo,target02_repeatNoise,np.arange(256),round(len(guitarSolo)/4))
+    
+    min_01 = np.amin(f_01)
+    max_01 = np.amax(f_01)
+    variacao_01 = max_01 - min_01
+    print("Variação "+str(round(variacao_01,4)))
+    
+    min_02 = np.amin(f_02)
+    max_02 = np.amax(f_02)
+    variacao_02 = max_02 - min_02
+    print("Variação "+str(round(variacao_02,4)))
     
 
+def ex_6_c():
+    samplerate, guitarSolo = wavfile.read("data\\guitarSolo.wav")
+    
+    for i in range(1,8):
+        samplerate, song__ = wavfile.read("data\\Song0"+str(i)+".wav")
+    
+        sorted_infMut = np.sort(ex_6_a(guitarSolo,song__,np.arange(256),round(len(guitarSolo))))
+        maxValue = sorted_infMut[-1]
+        print("Valor máximo da informação mútua no fihcheiro Song0"+str(i)+".wav é : "+str(maxValue))
+        
+  
+ 
 if __name__ == "__main__":
     # ex_1()
     # ex_2()
-    #ex_3_and_ex_4("data\\homer.bmp",1)
+    #ex_3_and_ex_4("data\\english.txt",0)
+    #ex_3_and_ex_4("data\\english.txt",1)
 
-    ex_5(["a","b","a","b","e","f"])
-    #print(list(set(["a","a","a","b","b","c"])))
+    #ex_5("data\\english.txt") 
+
+    query = np.array([2,6,4,10,5,9,5,8,0,8])
+    target = np.array([6,8,9,7,2,4,9,9,4,9,1,4,8,0,1,2,2,6,3,2,0,7,4,9,5,4,8,5,2,7,8,0,7,4,8,5,7,4,3,2,2,7,3,5,2,7,4,9,9,6])
+    alfabeto = [0,1,2,3,4,5,6,7,8,9,10]
+    print(ex_6_a(query,target,alfabeto,1))
+    #ex_6_b()
+    #ex_6_c()
     
-    #ex_6_a([2,6,4,10,5,9,5,8,0,8],[6,8,9,7,2,4,9,9,4,9,1,4,8,0,1,2,2,6,3,2,0,7,4,9,5,4,8,5,2,7,8,0,7,4,8,5,7,4,3,2,2,7,3,5,2,7,4,9,9,6],[0,1,2,3,4,5,6,7,8,9,10],1 )
